05d360f04c3e033b55d4ea76cc0c76ca64ee4843
Egor Frygin <efrygin@mail.ru>
2022-09-18 14:26:35 +0300

    Added frontend UI

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,6 +1,48 @@
+import {useState, useEffect} from 'react'
+import {FaSignInAlt} from 'react-icons/fa'
+
 function Login() {
+  const [formData, setFormData]: any = useState({
+    email: '',
+    password: '',
+  })
+
+  const {email, password} = formData
+
+  const onChange = (e: any) => {
+    setFormData((prevState: any) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  const onSubmit = (e: any) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>Login</div>
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Log in to your profile and start shopping</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input type="email" className="form-control" id='email' name='email' value={email} placeholder='Enter your email' onChange={onChange}/>
+          </div>
+          <div className="form-group">
+            <input type="password" className="form-control" id='password' name='password' value={password} placeholder='Enter password' onChange={onChange}/>
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,6 +1,64 @@
+import {useState, useEffect} from 'react'
+import {FaUser} from 'react-icons/fa'
+
 function Register() {
+  const [formData, setFormData]: any = useState({
+    firstname: '',
+    lastname: '',
+    username: '',
+    email: '',
+    password: '',
+    password2: ''
+  })
+
+  const {firstname, lastname, username, email, password, password2} = formData
+
+  const onChange = (e: any) => {
+    setFormData((prevState: any) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  const onSubmit = (e: any) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>Register</div>
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Create an account</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input type="text" className="form-control" id='firstname' name='firstname' value={firstname} placeholder='Enter your first name' onChange={onChange}/>
+          </div>
+          <div className="form-group">
+            <input type="text" className="form-control" id='lastname' name='lastname' value={lastname} placeholder='Enter your last name' onChange={onChange}/>
+          </div>
+          <div className="form-group">
+            <input type="text" className="form-control" id='username' name='username' value={username} placeholder='Enter your username' onChange={onChange}/>
+          </div>
+          <div className="form-group">
+            <input type="email" className="form-control" id='email' name='email' value={email} placeholder='Enter your email' onChange={onChange}/>
+          </div>
+          <div className="form-group">
+            <input type="password" className="form-control" id='password' name='password' value={password} placeholder='Enter password' onChange={onChange}/>
+          </div>
+          <div className="form-group">
+            <input type="password" className="form-control" id='password2' name='password2' value={password2} placeholder='Confirm password' onChange={onChange}/>
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
